da74f3711049ee296a6203b642e4446dd8eeb992
Richard Lucas <richardluca@gmail.com>
2023-06-04 23:57:51 +0200

    Register.jsx finished

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,3 +1,5 @@
+import Add from "../img/AddAvatar.png";
+
 const Register = () => {
   return (
     <div className="formContainer">
@@ -8,7 +10,11 @@ const Register = () => {
           <input type="text" placeholder="display name" />
           <input type="email" placeholder="email" />
           <input type="password" placeholder="password" />
-          <input type="file" />
+          <input style={{ display: "none" }} type="file" id="file" />
+          <label htmlFor="file">
+            <img src={Add} alt="" />
+            <span>Add an avatar</span>
+          </label>
           <button>Sign up</button>
         </form>
         <p>Do you have an account? Login</p>
