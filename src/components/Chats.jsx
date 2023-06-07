4286a3f53da3b78ab2510b3c36d32c831e280f53
Richard Lucas <richardluca@gmail.com>
2023-06-08 00:15:41 +0200

    Search and Sidebar.jsx done. Chats.jsx yet to finish

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,5 +1,18 @@
 const Chats = () => {
-  return <h1>Chats</h1>;
+  return (
+    <div className="chats">
+      <div className="userChat">
+        <img
+          src="https://images.pexels.com/photos/17123250/pexels-photo-17123250.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>Jane</span>
+          <p>Hello</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Chats;
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,5 +1,20 @@
 const Search = () => {
-    return <h1>Search</h1>
+  return (
+    <div className="search">
+      <div className="searchForm">
+        <input type="text" placeholder="find user" />
+      </div>
+      <div className="userChat">
+        <img
+          src="https://images.pexels.com/photos/17123250/pexels-photo-17123250.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>Jane</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Search;
