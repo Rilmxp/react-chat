00d035840cc9d0eeffa164c6d03eb20b604abd02
Richard Lucas <richardluca@gmail.com>
2023-06-15 23:00:28 +0200

    message and chat layout  updated. yet to finish

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,5 +1,22 @@
 const Message = () => {
-  return <p>Message</p>;
+  return (
+    <div className="message owner">
+      <div className="messageInfo">
+        <img
+          src="https://images.pexels.com/photos/17123250/pexels-photo-17123250.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          alt=""
+        />
+        <span>Just now</span>
+      </div>
+      <div className="messageContent">
+        <p>hello</p>
+        <img
+          src="https://images.pexels.com/photos/17123250/pexels-photo-17123250.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          alt=""
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Message;
